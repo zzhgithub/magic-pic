9b5b178dff2fe17650a5334b4b0a1b410c1d2df2
LdBeth <andpuke@foxmail.com>
2018-04-21 02:38:48 -0700

    Style cleanup
    Please feel free to cherry pick any changes

--- matrix.lisp
+++ matrix.lisp
@@ -7,39 +7,45 @@
   (setf *png-object* (png-read:read-png-file file)))
 
 ;; todo can be improve
-(defun print-png-info ()
-      (let ((width (png-read:width *png-object*))
-            (height (png-read:height *png-object*))
-            (bit-depth (png-read:bit-depth *png-object*)))
-        (format t "width: ~A~%height: ~A~%bit-depth: ~A~%" width height bit-depth)))
+(defmethod print-object ((obj png-read:png-state) stream)
+  (let ((width (png-read:width obj))
+        (height (png-read:height obj))
+        (bit-depth (png-read:bit-depth obj)))
+    (format stream "#<PNG: width: ~A height: ~A bit-depth: ~A>"
+            width height bit-depth)))
 
-(defun save-image-data (&optional (file "test"))
+(defun save-image-data (&optional (file #P"test"))
   (with-open-file (out file
                        :direction :output
                        :if-exists :supersede)
-    (print (png-read:image-data *png-object*) out)))
+    (princ (png-read:image-data *png-object*) out)))
 
-(defun check-object-and-do-funcation (fn)
-  (if (eql nil *png-object*)
-      (format t "we never hava png-object.please use funcation (set-png-object *file*) to set frist")
-      (funcall fn)))
+(defun check-object ()
+  (assert (typep *png-object* 'png-read:png-state)
+          (*png-object*)
+          "`*png-object*' wrong type: ~S.
+Please use funcation `set-png-object' to assign a value." *png-object*))
 
 (defun safe-print-png-info ()
-  (check-object-and-do-funcation #'print-png-info))
+  (check-object)
+  (print-png-info))
 
 (defun safe-save-image-data ()
-  (check-object-and-do-funcation #'save-image-data))
+  (check-object)
+  (save-image-data))
 
 ;; 这里使用匿名函数是重复的
 (defun get-size ()
-  (let ((fn #'(lambda () (list (png-read:width *png-object*) (png-read:height *png-object*)))))
-    (check-object-and-do-funcation fn)))
+  (check-object)
+  (funcall (lambda ()
+             (list (png-read:width *png-object*)
+                   (png-read:height *png-object*)))))
 
-(defun png->bitmap ()
-  (let ((image (png-read:image-data *png-object*))
+(defun png->bitmap (png)
+  (let ((image (png-read:image-data png))
         (bitmap (make-array (get-size) :initial-element 0)));; 接下来应该循环这个数组了
-    (dotimes (i (png-read:width *png-object*))
-      (dotimes (j (png-read:height *png-object*))
+    (dotimes (i (png-read:width png))
+      (dotimes (j (png-read:height png))
         (let ((s 0))
           (dotimes (k 3)
             (setf s (+ s (aref image i j k))))
@@ -48,7 +54,8 @@
 ;; safe-png->bitmap
 
 (defun safe-png->bitmap ()
-  (check-object-and-do-funcation #'png->bitmap))
+  (check-object)
+  (png->bitmap *png-object*))
 
 
 ;;现在需要设计算法解决这个问题
@@ -60,7 +67,24 @@
 ;;  . , _ - ~ : ! + = a 0 b % E H V & # M @
 
 ;; 这个定义一定是可以使使用 宏的~~ 这需要改造 a 0 b % E H V & # M @
+(defparameter *char-hash-map*
+  #.(let ((hash (make-hash-table :size 25))
+          (dict '("." "," "_" "-" "~" ":" "!"
+                  "+" "=" "a" "0" "b" "%" "V"
+                  "H" "E" "&" "#" "M" "@")))
+      (loop for i from (1- (length dict)) downto 0
+            and val in dict
+            do (setf (gethash i hash) val)
+            finally (return hash))))
+
 (defun get-string (i)
+  (declare (optimize (speed 3)) (fixnum i))
+  (or (gethash i *char-hash-map*) "@"))
+
+;; NOTE: there is no significant performance
+;; difference between `get-string' & `get-string1'
+#|
+(defun get-string1 (i)
   (case i
     (19 ".")
     (18 ",")
@@ -82,7 +106,7 @@
     (2 "#")
     (1 "M")
     (0 "@")
-    (otherwise "@")))
+    (otherwise "@")))|#
 
 (defun get-string-bitmap (bitmap)
   (let ((string-bitmap (make-array (get-size) :initial-element ".")))
